858803891787d1ece1b31689fce488ec7fe0c50b
Faiyaz Awsaf <faiyazawsaf11@gmail.com>
2025-12-22 23:13:26 +0600

    gemini api connection

--- backend/apps/evaluation/services.py
+++ backend/apps/evaluation/services.py
@@ -42,6 +42,7 @@ def get_gemini_model():
     genai.configure(api_key=api_key)
 
     model_preference_list = [
+        'gemini-2.5-pro',
         'gemini-1.5-pro-latest',
         'gemini-1.5-pro',
         'gemini-1.5-flash-latest',
